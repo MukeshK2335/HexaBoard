08b4db8419ec3481ac4bb346283a4af68a6a5464
Mukesh K <kmukeshkumar2335@gmail.com>
2025-07-23 18:07:57 +0530

    Altered

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RoleSwitcherLogin from './Components/RoleSwitcherLogin.jsx';
 import AdminDashboard from './Components/AdminDashboard.jsx';
 import FresherDashboard from './Components/FresherDashboard.jsx';
+import './App.css';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<RoleSwitcherLogin />} />
+                <Route
+                    path="/"
+                    element={
+                        <div className="outer-box">
+                            <div className="inner-wrapper">
+                                <RoleSwitcherLogin />
+                            </div>
+                        </div>
+                    }
+                />
                 <Route path="/admin" element={<AdminDashboard />} />
                 <Route path="/fresher" element={<FresherDashboard />} />
             </Routes>
